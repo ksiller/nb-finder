354a62e00ad0b67550ab37f87c4337bb7669aa74
Alan Bergland <aob2x@virginia.edu>
2023-10-13 14:01:15 -0400

    updates

--- cellpose_script.sh
+++ cellpose_script.sh
@@ -11,7 +11,7 @@
 #SBATCH --account berglandlab
 
 ### run as: sbatch --array=2 ~/nb-finder/cellpose_script.sh
-### sacct -j 54069950
+### sacct -j 54071009
 ### cat /scratch/aob2x/logs/demo_1.54068919_2.out
 # ijob -A berglandlab -c10 -p gpu --mem=64G --gres=gpu
 ### SLURM_ARRAY_TASK_ID=1
@@ -113,7 +113,5 @@
 ### save results and clean up
   ls -lhd ${tmpdir}/*
   cat ${tmpdir}/*.nMasks > ${results_path}/${img_stem}.nMasks.txt
-  # cp ${tmpdir}/*.nMasks ${results_path}/.
-  # cp ${tmpdir}/*seg.npy ${results_path}/.
-
+  
   rm -fr ${tmpdir}
